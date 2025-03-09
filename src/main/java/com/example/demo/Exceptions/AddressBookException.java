66fac737e365808d40380c4ca3ef803220dba6b4
ry129 <0111cs211146.riya@gmail.com>
2025-03-09 19:15:25 +0530

    [Riya]Add. Custom Eexception Handled

---
+++ src/main/java/com/example/demo/Exceptions/AddressBookException.java
@@ -0,0 +1,8 @@
+package com.example.demo.Exceptions;
+
+public class AddressBookException extends RuntimeException{
+    public AddressBookException(String message){
+        super(message);
+
+    }
+}
--- src/main/java/com/example/demo/Exceptions/AddressBookExceptionalHandler.java
+++ src/main/java/com/example/demo/Exceptions/AddressBookExceptionalHandler.java
@@ -3,22 +3,27 @@ package com.example.demo.Exceptions;
 import com.example.demo.DTO.ResponseDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.ObjectError;
+
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.util.List;
-import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class AddressBookExceptionalHandler {
 
+    // Handle generic validation exceptions (e.g., validation errors)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ResponseDTO> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception){
-        List<ObjectError> errorList = exception.getBindingResult().getAllErrors();
-        List<String> errMesg = errorList.stream().map(objErr -> objErr.getDefaultMessage()).collect(Collectors.toList());
-        ResponseDTO responseDTO=new ResponseDTO("Exception while processing REST Request",errMesg);
-        return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ResponseDTO> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        ResponseDTO responseDTO = new ResponseDTO("Validation Error", exception.getBindingResult().getFieldError().getDefaultMessage());
+        return new ResponseEntity<>(responseDTO, HttpStatus.BAD_REQUEST);
+    }
+
+    // Handle custom AddressBookException
+    @ExceptionHandler(AddressBookException.class)
+    public ResponseEntity<ResponseDTO> handleAddressBookException(AddressBookException exception) {
+        ResponseDTO responseDTO = new ResponseDTO("Exception while processing REST Request", exception.getMessage());
+        return new ResponseEntity<>(responseDTO, HttpStatus.BAD_REQUEST);
     }
 }
+
